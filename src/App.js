ca59cce5139d682beb718b4be49a27104b7370cb
Lonely <juho772@gmail.com>
2023-01-22 15:33:15 +0200

    Testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import About from './Pages/about';
 import Frontpage from './Pages/frontpage';
-import { Routes, Route, HashRouter, BrowserRouter} from 'react-router-dom'
+import { Routes, Route, BrowserRouter} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Projects from './Pages/projects';
 
@@ -9,11 +9,11 @@ const App = () => {
   return (
     <div className="">
       <Navbar />
-      <BrowserRouter> 
+      <BrowserRouter basename="/homepage"> 
         <Routes>
-          <Route exact path="/homepage" element={<Frontpage />} > </Route>
-          <Route path="/homepage/about" element={<About />} > </Route>
-          <Route path="/homepage/projects" element={<Projects />} > </Route>
+          <Route exact path="/" element={<Frontpage />} > </Route>
+          <Route path="/about" element={<About />} > </Route>
+          <Route path="/projects" element={<Projects />} > </Route>
         </Routes>
       </BrowserRouter> 
     </div>
