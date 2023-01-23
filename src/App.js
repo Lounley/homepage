1f9a60ae020793abc9d3c59efcd0a38f231b1e71
Lonely <juho772@gmail.com>
2023-01-23 02:44:31 +0200

    Korjattiin Navbar toimimaan hashrouterilla

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import React from "react";
 import About from './Pages/about';
 import Frontpage from './Pages/frontpage';
-import { Routes, Route, BrowserRouter} from 'react-router-dom'
+import { Routes, Route} from 'react-router';
 import Navbar from './components/Navbar';
 import Projects from './Pages/projects';
 
@@ -9,13 +10,11 @@ const App = () => {
   return (
     <div className="">
       <Navbar />
-      <BrowserRouter basename="/homepage"> 
-        <Routes>
-          <Route exact path="/" element={<Frontpage />} > </Route>
-          <Route path="/about" element={<About />} > </Route>
-          <Route path="/projects" element={<Projects />} > </Route>
-        </Routes>
-      </BrowserRouter> 
+      <Routes>
+        <Route exact path="/" element={<Frontpage />} /> 
+        <Route exact path="/about" element={<About />} /> 
+        <Route exact path="/projects" element={<Projects />} /> 
+      </Routes>
     </div>
 
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
     return (
       <>
         <div className="from- bg-gradient-to-r from-indigo-400 to-sky-100 sticky top-0 z-50 space-x-2 p-1">
-          <TopbarLink link="/homepage" text="Etusivu"/>        
+          <TopbarLocalLink link="/" text="Etusivu"/>        
           <TopbarLink link="https://github.com/Lounley" text="Github"/>    
           <TopbarLink link="https://www.linkedin.com/in/juho-suomela-1b698b194/" text="Linkedin"/>   
-          <TopbarLink link="/homepage/projects" text="Projects"/>           
+          <TopbarLocalLink link="/projects" text="Projects"/>           
           <TopbarLink link="https://www.instagram.com/juhd0/" text="Instagram"/> 
           <TopbarLink link="https://www.youtube.com/@vaasanlaskuvarjokerho1110" text="Skydive"/>
-          <TopbarLink link="/homepage/about" text="About me"/>
+          <TopbarLocalLink link="/about" text="About me"/>
 
         </div>
       </>
@@ -22,6 +23,12 @@ const Navbar = () => {
   const TopbarLink = (props) => {
     return (
       <a href={props.link} className="text-4xl bg-clip-text bg-gradient-to-r from-blue-800 to-purple-800 rounded text-transparent font-bold no-underline font-bold px-1">{props.text}</a>
+    )
+  }
+  
+  const TopbarLocalLink = (props) => {
+    return (
+      <Link to={props.link} className="text-4xl bg-clip-text bg-gradient-to-r from-blue-800 to-purple-800 rounded text-transparent font-bold no-underline font-bold px-1">{props.text}</Link>
     )
   }
   
